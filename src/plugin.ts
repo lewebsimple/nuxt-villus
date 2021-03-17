0219c9d5f4af365c5abc5e0cd3c67a340977f2be
Pascal Martineau <pascal@lewebsimple.ca>
2021-03-17 14:00:23 -0400

    feat: Supprot batch, multipart and subscriptions

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,23 +1,28 @@
 import { Plugin } from "@nuxt/types";
-import { createClient, cache, dedup, fetch } from "villus";
-import $fetch from "cross-fetch";
+import { createClient, cache, dedup, handleSubscriptions } from "villus";
+import { batch } from "@villus/batch";
+import { multipart } from "@villus/multipart";
+import { SubscriptionClient } from "graphql-subscriptions-client";
+import fetch from "cross-fetch";
 
 // Get options from template without triggering ESLint
 const options = JSON.parse(`<%= JSON.stringify(options, null, 2) %>`);
 
 const villusPlugin: Plugin = (context, inject) => {
   // Default Villus plugins
-  const plugins = [
-    cache(),
-    dedup(),
-    fetch({
-      fetch: $fetch,
-    }),
-  ];
+  const plugins = [];
 
   if (!options.httpEndpoint) {
     throw new Error("GraphQL HTTP endpoint missing.");
   }
+
+  if (process.client && options.wsEndpoint) {
+    // Subscriptions handling
+    const subscriptionClient = new SubscriptionClient(options.wsEndpoint, { reconnect: true });
+    const subscriptions = handleSubscriptions((operation) => subscriptionClient.request(operation));
+    plugins.push(subscriptions);
+  }
+  plugins.push(...[multipart(), cache(), dedup(), batch({ fetch })]);
 
   // Create Villus client
   const villus = createClient({
